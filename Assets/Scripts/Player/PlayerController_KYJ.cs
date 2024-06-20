4c943cfd819a60cb8331dbf5b3f599c2e5384a42
chuff341k <yejungo44@gmail.com>
2024-06-20 21:46:32 +0900

    Fix : 플레이어 움직임 수정

--- Assets/Scripts/Player/PlayerController_KYJ.cs
+++ Assets/Scripts/Player/PlayerController_KYJ.cs
@@ -1,10 +1,33 @@
 using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class PlayerController_KYJ : MonoBehaviour
 {
     public Action inventory;
+
+    private Vector2 moveDirection;
+    private float moveSpeed = 4f;
+
+    private void Update()
+    {
+        bool hasControl = (moveDirection != Vector2.zero);
+        if (hasControl)
+        {
+            transform.rotation = Quaternion.LookRotation(moveDirection);
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Vector2 input = context.ReadValue<Vector2>();
+        if (input != null )
+        {
+            moveDirection = new Vector3(input.x, input.y, 0f);
+        }
+    }
 
     public void OnInventory(InputAction.CallbackContext context)
     {
